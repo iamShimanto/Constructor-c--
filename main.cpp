ba0e062068293992195bda4e8f8809955fa25c07
I-Am-Shimanto <shimantowebdeveloper@gmail.com>
2025-04-28 00:06:24 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car{
+    public:
+    string brand;
+    string model;
+    int year;
+    Car(string x , string y , int z);
+};
+
+Car::Car(string x , string y , int z){
+    brand = x;
+    model = y;
+    year = z;
+}
+
+int main(){
+    string brand, model;
+    int year;
+    cout << "Enter your car brand : ";
+    getline(cin, brand);
+    cout << "Enter your car model : ";
+    getline(cin, model);
+    cout << "Enter your car Year : ";
+    cin >> year;
+
+    Car mycar(brand, model , year);
+    cout << endl;
+
+    cout << "Your car details is : " << endl
+         << "Brand : " << mycar.brand << endl
+         << "Model : " << mycar.model << endl
+         << "Year : " << mycar.year << endl;
+
+    return 0;
+}
